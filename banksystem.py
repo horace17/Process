129abcab38058ebc2f7e5f175e5f0348b7473bdc
horace17 <hocedrix2010@gmail.com>
2023-01-01 03:47:49 +0300

    Updated bank system

--- banksystem.py
+++ banksystem.py
@@ -11,3 +11,27 @@ class User:
         print("age ",self.age)
         print("gender ", self.gender)
 
+class Bank(User):
+    def __init__(self,name,age,gender):
+        super().__init__(name,age,gender):
+            self.balance = 0
+
+    def deposit(self, amount):
+        self.amount = amount
+        self.balance = self.balance + self.amount
+        print("The account balance has been updated : $", self.balance)
+
+    def withdrawal(self, amount):
+        self.amount = amount
+        if self.amount > self.balance:
+            print("Insufficient funds. Balance available is : $", self.balance)
+        else:
+            self.balance = self.balance - self.amount
+            print("Account balance has been updated: $", self.balance)
+
+    def view_balance(self):
+        self.show_details()
+        print("Account balance is $", self.balance)
+
+
+
